6fe684d79c426ca979a0bd159733856cb5cd9f26
gil.regalado <gil.regalado@netzontech.com>
2024-07-25 20:39:33 +0800

    chore: use dialog mechanism instead of manual overlay

--- src/components/gallery/buttons/gallery-edit-search-bar-button.tsx
+++ src/components/gallery/buttons/gallery-edit-search-bar-button.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useRef } from 'react';
 import icoSearch from '../../../assets/ico-search.svg';
 import { colors } from '../../../consts/colors';
 
@@ -10,8 +10,9 @@ export type GalleryEditSearchBarButtonProps = {
 export const GalleryEditSearchBarButton: React.FC<
   GalleryEditSearchBarButtonProps
 > = ({ width, height }) => {
+  const searchDialogRef = useRef<HTMLDialogElement>(null);
+  const searchContainerRef = useRef<HTMLDivElement>(null);
   const searchBarIcoSize = 14;
-  const [isSearchDialogVisible, setIsSearchDialogVisible] = useState(false);
 
   const randomActions = [
     'Find Annotation',
@@ -39,7 +40,6 @@ export const GalleryEditSearchBarButton: React.FC<
       }}
     >
       <div
-        className="ns"
         style={{
           display: 'flex',
           justifyContent: 'center',
@@ -48,9 +48,10 @@ export const GalleryEditSearchBarButton: React.FC<
           borderRadius: 4,
           width: width,
           height: height,
+          backgroundColor: colors.offWhite,
         }}
         onClick={() => {
-          setIsSearchDialogVisible((prev) => !prev);
+          searchDialogRef.current?.showModal();
         }}
       >
         <img
@@ -66,34 +67,47 @@ export const GalleryEditSearchBarButton: React.FC<
           search
         </span>
       </div>
-      {isSearchDialogVisible && (
-        <div
-          style={{
-            zIndex: 1,
-            width: width,
-            height: 400,
-            backgroundColor: colors.offWhite,
-            border: '1px solid #ccc',
-            position: 'relative',
-            top: -height,
-            display: 'flex',
-            flexDirection: 'column',
-            borderRadius: 4,
-            boxShadow: '0 0 10px rgba(0, 0, 0, 0.5)',
-          }}
-          onMouseLeave={() => {
-            setIsSearchDialogVisible(false);
-          }}
-        >
+      <dialog
+        ref={searchDialogRef}
+        onClick={(e) => {
+          if (searchDialogRef.current && searchContainerRef.current) {
+            const rect = searchContainerRef.current.getBoundingClientRect();
+            if (
+              e.clientX < rect.left ||
+              e.clientX > rect.right ||
+              e.clientY < rect.top ||
+              e.clientY > rect.bottom
+            ) {
+              searchDialogRef.current.close();
+            }
+          }
+        }}
+        style={{
+          width: width - 2,
+          padding: 0,
+          margin: 0,
+          left: `calc(50% - ${width / 2}px)`,
+          top: 10,
+          // top: 50,
+          backgroundColor: colors.offWhite,
+          border: '1px solid #ccc',
+          // border: 'none',
+          borderRadius: 4,
+          boxShadow: '0 0 10px rgba(0, 0, 0, 0.5)',
+        }}
+      >
+        <div ref={searchContainerRef}>
           <div
             style={{
-              width: width - 4,
               padding: 4,
             }}
           >
             <input
               style={{
-                width: width - 16,
+                width: width - 14,
+                height: 20,
+                padding: 0,
+                margin: 0,
               }}
               type="text"
               defaultValue={'dfsdf'}
@@ -114,7 +128,7 @@ export const GalleryEditSearchBarButton: React.FC<
             );
           })}
         </div>
-      )}
+      </dialog>
     </div>
   );
 };
